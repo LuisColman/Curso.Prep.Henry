490ad16a0ed9028fbc61762d2f25f7829ba219ce
Luis Colman <ljcolman@gmail.com>
2021-12-01 09:09:16 -0300

    Agregado archivos modificado homework 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -188,7 +188,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  var aleatorio = Math.round(Math.random()); 
+  var aleatorio = Math.random(); 
 return aleatorio;
 }
 
